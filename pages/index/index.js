dfaf883b7c3c0fc992a171a86c626e2582549e8c
hjd <hjd@yht.com>
2020-07-15 17:55:56 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,126 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 被点击导航菜单的索引
+    currentIndexNav: 0,
+    // 首页导航数据
+    navList: []
+  },
+  // 点击首页导航按钮
+    activeNav(e){
+      this.setData({
+        currentIndexNav: e.target.dataset.index
+      })
+    },
+
+  // 获取首页导航数据
+  getNavList(){
+    let that = this
+    //  利用小程序内置发起请求的方法
+    wx.request({
+      url: 'https://www.fastmock.site/mock/c8b94bc7b5f6687ccfe99d2c4132803f/bi/navList',
+      success(res){
+        // console.log(res);
+        if(res.data.code===0){
+          that.setData({
+            navList:res.data.data.navList
+          })
+        }
+      }
+    })
+  },
+  // 获取轮播图数据
+ 
+  getSwiperList() {
+    let that = this
+    wx.request({
+      url: 'https://www.fastmock.site/mock/c8b94bc7b5f6687ccfe99d2c4132803f/bi/swiperList',
+      success(res) {
+        // console.log(res)
+        if(res.data.code===0){
+          that.setData({
+            swiperList:res.data.data.swiperList
+          })
+        }
+      }
+    })
+  },
+  // 获取视屏列表
+  getVideosList(){
+    let that = this;
+    wx.request({
+      url: 'https://www.fastmock.site/mock/c8b94bc7b5f6687ccfe99d2c4132803f/bi/VideosList',
+      success(res){
+        if (res.data.code===0){
+          that.setData({
+            videosList:res.data.data.videosList
+          })
+        }
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 1.获取首页导航数据
+    this.getNavList();
+    // 2.获取轮播图数据
+    this.getSwiperList();
+    // 3.调用获取视频列表函数
+    this.getVideosList();
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
